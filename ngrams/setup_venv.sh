a0963a7656c5c7884cd4db810b35bc3a455bf267
Alex Rudnick <alex.rudnick@gmail.com>
2013-12-05 01:44:55 -0500

    make sure we have relevant nltk packages

--- ngrams/setup_venv.sh
+++ ngrams/setup_venv.sh
@@ -2,3 +2,5 @@ virtualenv -p /usr/bin/python3 venv
 . venv/bin/activate
 pip install https://github.com/nltk/nltk/tarball/develop
 pip install pyyaml
+python3 -m nltk.downloader cmudict
+python3 -m nltk.downloader punkt
